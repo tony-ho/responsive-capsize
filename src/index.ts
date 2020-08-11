5dd46d06a5764a9d3ca8136538b7315c196f0e15
Tony Ho <tony-ho@users.noreply.github.com>
2020-08-11 22:23:27 +1000

    feat: add Responsive Capsize module

---
+++ src/index.ts
@@ -0,0 +1,139 @@
+import capsize, { CapsizeStyles, FontMetrics } from 'capsize'
+import zipWith from 'lodash.zipwith'
+
+type CapHeightWithLeading = {
+  capHeight: number[]
+  leading: number[]
+  fontMetrics: FontMetrics
+}
+
+type CapHeightWithLineGap = {
+  capHeight: number[]
+  lineGap: number[]
+  fontMetrics: FontMetrics
+}
+
+type FontSizeWithLeading = {
+  fontSize: number[]
+  leading: number[]
+  fontMetrics: FontMetrics
+}
+
+type FontSizeWithLineGap = {
+  fontSize: number[]
+  lineGap: number[]
+  fontMetrics: FontMetrics
+}
+
+export type ResponsiveCapsizeOptions =
+  | CapHeightWithLineGap
+  | CapHeightWithLeading
+  | FontSizeWithLineGap
+  | FontSizeWithLeading
+
+export type ResponsiveCapsizeStyles = {
+  fontSize: string[]
+  lineHeight: string[]
+  padding: string
+  '::before': {
+    content: string
+    marginTop: string[]
+    display: string
+    height: number
+  }
+  '::after': {
+    content: string
+    marginBottom: string[]
+    display: string
+    height: number
+  }
+}
+
+export default function responsiveCapsize(
+  options: ResponsiveCapsizeOptions
+): ResponsiveCapsizeStyles {
+  const { fontMetrics } = options
+
+  if ('capHeight' in options) {
+    const { capHeight } = options
+    if (capHeight.length < 1) {
+      throw new Error('Must provide at least one `capHeight`')
+    }
+    if ('leading' in options) {
+      const { leading } = options
+      return mergeCapsizeStyles(
+        zipWith(capHeight, leading, (c, l) =>
+          capsize({
+            capHeight: c ?? capHeight[capHeight.length - 1],
+            leading:
+              l ??
+              (leading.length > 0 ? leading[leading.length - 1] : undefined),
+            fontMetrics
+          })
+        )
+      )
+    }
+
+    const { lineGap } = options
+    if (lineGap.length < 1) {
+      throw new Error('Must provide at least one `lineGap`')
+    }
+    return mergeCapsizeStyles(
+      zipWith(capHeight, lineGap, (c, l) =>
+        capsize({
+          capHeight: c ?? capHeight[capHeight.length - 1],
+          lineGap: l ?? lineGap[lineGap.length - 1],
+          fontMetrics
+        })
+      )
+    )
+  }
+
+  const { fontSize } = options
+  if (fontSize.length < 1) {
+    throw new Error('Must provide at least one `fontSize`')
+  }
+  if ('leading' in options) {
+    const { leading } = options
+    return mergeCapsizeStyles(
+      zipWith(fontSize, leading, (f, l) =>
+        capsize({
+          fontSize: f ?? fontSize[fontSize.length - 1],
+          leading:
+            l ?? (leading.length > 0 ? leading[leading.length - 1] : undefined),
+          fontMetrics
+        })
+      )
+    )
+  }
+
+  const { lineGap } = options
+  if (lineGap.length < 1) {
+    throw new Error('Must provide at least one `lineGap`')
+  }
+  return mergeCapsizeStyles(
+    zipWith(fontSize, lineGap, (f, l) =>
+      capsize({
+        fontSize: f ?? fontSize[fontSize.length - 1],
+        lineGap: l ?? lineGap[lineGap.length - 1],
+        fontMetrics
+      })
+    )
+  )
+}
+
+function mergeCapsizeStyles(styles: CapsizeStyles[]): ResponsiveCapsizeStyles {
+  return {
+    ...styles[0],
+    fontSize: styles.map(s => s.fontSize),
+    lineHeight: styles.map(s => s.lineHeight),
+    '::before': {
+      ...styles[0]['::before'],
+      marginTop: styles.map(s => s['::before'].marginTop)
+    },
+    '::after': {
+      ...styles[0]['::after'],
+      marginBottom: styles.map(s => s['::after'].marginBottom)
+    }
+  }
+}
